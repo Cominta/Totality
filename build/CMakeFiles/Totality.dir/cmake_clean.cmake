d64021e6e666aab828b4740c7dd0231f7d2309c6
unknown <romanitstudents@gmail.com>
2023-03-01 00:16:10 +0200

    init

---
+++ build/CMakeFiles/Totality.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/Roman/Desktop/Totality/output/Totality.exe"
+  "C:/Users/Roman/Desktop/Totality/output/Totality.exe.manifest"
+  "C:/Users/Roman/Desktop/Totality/output/Totality.pdb"
+  "C:/Users/Roman/Desktop/Totality/output/libTotality.dll.a"
+  "CMakeFiles/Totality.dir/src/main.cpp.obj"
+  "CMakeFiles/Totality.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Totality.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ genCmake/path.h
@@ -0,0 +1,7 @@
+#ifndef PATH_H
+#define PATH_H
+
+const char* gSource = "C:/Users/Roman/Desktop/Totality";
+const char* gBinary = "C:/Users/Roman/Desktop/Totality/build";
+
+#endif
