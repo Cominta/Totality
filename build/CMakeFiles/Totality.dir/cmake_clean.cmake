38906a177d1e6d22e4a5438839eca9028a0565cc
kotlj <crotlj6@gmail.com>
2023-03-05 01:00:27 +0200

    фикс2

---
+++ build/CMakeFiles/Totality.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Totality.dir/src/BaseUnit.cpp.obj"
+  "CMakeFiles/Totality.dir/src/BaseUnit.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/button.cpp.obj"
+  "CMakeFiles/Totality.dir/src/button.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/engine.cpp.obj"
+  "CMakeFiles/Totality.dir/src/engine.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/gameState.cpp.obj"
+  "CMakeFiles/Totality.dir/src/gameState.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/main.cpp.obj"
+  "CMakeFiles/Totality.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/mainState.cpp.obj"
+  "CMakeFiles/Totality.dir/src/mainState.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/state.cpp.obj"
+  "CMakeFiles/Totality.dir/src/state.cpp.obj.d"
+  "CMakeFiles/Totality.dir/src/tilemap.cpp.obj"
+  "CMakeFiles/Totality.dir/src/tilemap.cpp.obj.d"
+  "D:/Microsoft Visual Studio/VS Code repos/Totality/output/Totality.exe"
+  "D:/Microsoft Visual Studio/VS Code repos/Totality/output/Totality.exe.manifest"
+  "D:/Microsoft Visual Studio/VS Code repos/Totality/output/Totality.pdb"
+  "D:/Microsoft Visual Studio/VS Code repos/Totality/output/libTotality.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Totality.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
