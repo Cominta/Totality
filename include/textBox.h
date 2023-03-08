c66f9b5f92380303fc60c4284e90c4dce1caf333
Cominta <romanitstudents@gmail.com>
2023-03-08 18:34:48 +0200

    fixes

---
+++ include/textBox.h
@@ -0,0 +1,37 @@
+#pragma once 
+
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+class TextBox 
+{
+    private:
+        sf::RenderWindow* window;
+        sf::RectangleShape background;
+        sf::Text text;
+        sf::Font font;
+        sf::Color backgoundColor;
+        sf::Color backgoundColorOut;
+        std::string str;
+
+        bool active;
+        bool cursor;
+        int maxSymbols;
+
+        int x;
+        int y;
+
+        int width;
+        int height;
+
+        void isActive(bool mousePressedLeft, sf::Vector2f mousePosition);
+        void deleteLastChar();
+        void handle(int charTyped);
+
+    public:
+        TextBox(sf::RenderWindow* window, int width, int height, float x, float y);
+
+        void typedOn(sf::Event sfEvent);
+        void update(bool mousePressedLeft, sf::Vector2f mousePosition, bool backspace);
+        void render();
+};
